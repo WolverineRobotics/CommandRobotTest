6042831d85110d51d22d82da1ff4dcd59f8b7da7
Ouen <ouenramb@gmail.com>
2023-09-17 00:03:37 -0400

    a

--- swerve_robot_2024/src/main/java/frc/robot/commands/Drive/ForwardDriveCommand.java
+++ swerve_robot_2024/src/main/java/frc/robot/commands/Drive/ForwardDriveCommand.java
@@ -1,16 +1,38 @@
 package frc.robot.commands.Drive;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.InputSystem;
 
-public class ForwardDriveCommand {
+public class ForwardDriveCommand extends CommandBase {
+    private DriveSubsystem m_Subsystem;
+    private boolean fini = false;
+    private double distance;
+    private double speed = InputSystem.DriveSpeed();
 
-/*public class SetForwardDistanceCommand extends CommandBase {
-    PigeonIMU penisBalls = new PigeonIMU();
-
-    private final SetForwardDistanceSubsystem m_Subsystem;
-
-    public SetForwardDistanceCommand(DriveSubsystem subsystem){
+    public ForwardDriveCommand(DriveSubsystem subsystem, double _distance, double _speed){
         m_Subsystem = subsystem;
         addRequirements(subsystem);
+        _distance = distance;
+        _speed = speed;
     }
-}*/
 
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override 
+    public void execute() {
+        m_Subsystem.MoveStraight(speed);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return fini;
+    }
 }
