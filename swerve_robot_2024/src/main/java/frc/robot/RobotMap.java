e983a50a1b5b5b6b97e7c8fc65452b0b8ba8a59c
Austin <thisistotallyadiscordalt@gmail.com>
2023-09-18 12:22:18 -0400

    stole can id's

---
+++ swerve_robot_2024/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,31 @@
+package frc.robot;
+
+public class RobotMap {
+    public static int LEFT_MOTOR_1 = 11;
+    public static int LEFT_MOTOR_2 = 12;
+    public static int RIGHT_MOTOR_1 = 17;
+    public static int RIGHT_MOTOR_2 = 18;
+
+    public static int INTAKE_PIVOT_ID = 15;
+    public static int INTAKE_ID = 16;
+    
+    public static int SHOULDER_ENCODER_UPPER_LIMIT = 0;
+    public static int SHOULDER_ENCODER_LOWER_LIMIT = 0;
+
+    public static int ELBOW_ENCODER_UPPER_LIMIT = 0;
+    public static int ELBOW_ENCODER_LOWER_LIMIT = 0;
+
+    public static int ELEVATOR_MOTOR_ID = 10;
+    public static int ELEVATOR_MOTOR_ID_2 = 14;
+
+    public static int LIMIT_SWITCH_ID_1 = 7;
+    public static int LIMIT_SWITCH_ID_2 = 8;
+    
+    public static int L_PNEWUMATIC_ID_1 = 1;
+    public static int L_PNEWUMATIC_ID_2 = 0;
+    
+    public static int R_PNEWUMATIC_ID_1 = 0;
+    public static int R_PNEWUMATIC_ID_2 = 0;
+
+    public static double CYCLES_PER_INCH = 0.0722;
+}
